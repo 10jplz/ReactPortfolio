0c710a45c707a6699872c355a9f43fe060d013b6
Jose Contreras <10jplz@gmail.com>
2021-06-16 14:46:17 -0600

    Uploading Portfolio Page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,48 +1,36 @@
-import React from "react";
-import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-// import Image from 'react-bootstrap/Image'
-import CardDeck from 'react-bootstrap/CardDeck'
-import Card from 'react-bootstrap/Card'
+import React, { Component } from "react";
+import ProjectCard from "./assets/ProjectCard";
+import Wrapper from "./assets/Wrapper";
+import Title from "./assets/Title";
+import projects from "./assets/projects.json";
 
-function Portfolio () {
+class Portfolio extends Component {
+  // Setting this.state.friends to the project json array
+  state = {
+    projects
+  };
+
+
+  // Map over this.state.friends and render a Project Card component for each project object
+  render() {
     return (
-        <div>
-<Container>
-<CardDeck>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-
-  <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-  </Card.Body>
-</Card>
-
-  </CardDeck>
-
-  </Container>
-
-    
-  </div>
+      <Wrapper>
+        <Title>Project List</Title>
+        {this.state.projects.map(project => (
+          <ProjectCard
+            id={project.id}
+            key={project.id}
+            name={project.name}
+            image={project.image}
+            description={project.description}
+            location={project.location}
+            applink={project.applink}
+            gitrepo={project.gitrepo}
+          />
+        ))}
+      </Wrapper>
     );
+  }
 }
 
-export default Portfolio
+export default Portfolio;
